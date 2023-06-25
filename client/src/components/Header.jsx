8a5715d7b2ea1d48666d853b96ffb3ffcb3d6289
freetimezp <kraftukrnet@ukr.net>
2023-06-25 20:35:46 +0300

    08. js-social-11-pixabay. Toggle user menu.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 
 import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
@@ -7,17 +7,20 @@ import { firebaseAuth } from '../config/firebase.config';
 import { createNewUser } from '../sanity';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { SET_USER } from '../context/actions/userActions';
+import { SET_USER, SET_USER_NULL } from '../context/actions/userActions';
 
 import { Logo } from '../assets';
+import { mainMenu } from '../utils/supports';
 
 const Header = () => {
     const [color, setColor] = useState(false);
+    const [isMenu, setIsMenu] = useState(false);
 
     const provider = new GoogleAuthProvider();
     const dispatch = useDispatch();
 
     const user = useSelector(state => state.user);
+    const navigate = useNavigate();
 
     const changeColor = () => {
         if (typeof window !== 'undefined') {
@@ -43,6 +46,13 @@ const Header = () => {
         })
     }
 
+    const logOut = async () => {
+        await firebaseAuth.signOut().then(() => {
+            dispatch(SET_USER_NULL());
+            navigate('/', { replace: true });
+        });
+    }
+
     return (
         <header className={`fixed inset-x-0 px-8 sm:px-12 lg:px-32 xl:px-44 py-4 flex items-center justify-between
             z-50 translate-all duration-500 ${color ? 'bg-white/80' : 'bg-transparent'}`}>
@@ -50,7 +60,6 @@ const Header = () => {
                 <img
                     src={Logo} alt="logo"
                     className="w-24 h-auto object-contain"
-                    referrerPolicy='no-referrer'
                 />
             </Link>
 
@@ -58,7 +67,41 @@ const Header = () => {
                 {user ? (
                     <>
                         <div className='relative cursor-pointer'>
-                            <img src={user?.photoURL} alt="user" className='rounded-full w-10 h-10 object-cover' />
+                            <img
+                                src={user?.photoURL} alt="user"
+                                className='rounded-full w-10 h-10 object-cover'
+                                referrerPolicy='no-referrer'
+                                onClick={() => setIsMenu(!isMenu)}
+                            />
+
+                            {isMenu && (
+                                <div
+                                    className='absolute right-0 top-12 rounded-md shadow-md w-64 px-4 py-3 
+                                    bg-[#191b26] flex flex-col items-start justify-center gap-3'
+                                    onMouseLeave={() => setIsMenu(false)}
+                                >
+                                    <h2 className='text-gray-50 font-semibold'>
+                                        {user?.displayName}
+                                    </h2>
+                                    <div className='w-full h-[1px] bg-gray-700'></div>
+
+                                    {mainMenu && mainMenu.map((menu, i) => (
+                                        <Link
+                                            to={`/newPost/${menu.slug}`}
+                                            key={i}
+                                            className='text-base text-gray-300 transition-all duration-300 
+                                                                        hover:pl-1 hover:text-blue-300'
+                                        >
+                                            {menu?.name}
+                                        </Link>
+                                    ))}
+                                    <div className='w-full h-[1px] bg-gray-700'></div>
+
+                                    <p className='text-xl text-gray-300' onClick={logOut}>
+                                        Logout
+                                    </p>
+                                </div>
+                            )}
                         </div>
                     </>
                 ) : (
